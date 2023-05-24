825ca2c9f21bb676b922ccb238aaf25de26ef124
DANIEL ALEJANDRO ACERO ALMANZA <acerod@DAWN.COM>
2023-05-24 11:57:22 -0500

    pruebas react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    const handleSubmit =  async (e) => {
+        e.preventDefault();
 
-export default App;
+        try{
+            const response =  await fetch('http://127.0.0.1:8080/auth/login', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email, password
+                })
+            });
+            console.log(response);
+            if (!response.ok) {
+                throw new Error('Error en la solicitud', response.status);
+              }
+          
+              const jsonData = await response.json();
+              console.log(jsonData);
+        }
+        catch(error){
+            console.log('se genero un error en fetch', error);
+        }
+    };
+    return (
+        <div>
+            <h1>Authenticación en la API</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={email} onChange={(e)=> setEmail(e.target.value)} placeholder="Ingrese su usuario" />
+                <input type="text" value={password} onChange={(e)=> setPassword(e.target.value)} placeholder="Ingrese su contraseña" />
+                <button type="submit" class="btn btn-success">Ingresar</button>
+            </form>
+        </div>
+    )
+
+}
+ export default App;
